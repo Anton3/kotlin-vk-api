68832459f36e2608ea864402f1da0f688daaa3a5
Anton Zhilin <antonyzhilin@gmail.com>
2018-12-24 02:25:48 +0300

    OfflineReason parsing fix

--- vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/objects/OfflineReason.kt
+++ vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/objects/OfflineReason.kt
@@ -1,6 +1,7 @@
 package name.anton3.vkapi.methods.longpoll.objects
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import java.io.IOException
 
 enum class OfflineReason(val value: Int) {
     LEAVING(0),
@@ -9,8 +10,9 @@ enum class OfflineReason(val value: Int) {
     companion object {
         @JvmStatic
         @JsonCreator
-        fun parse(value: Int): OfflineReason? {
+        fun parse(value: Int): OfflineReason {
             return OfflineReason.values().find { it.value == value }
+                ?: throw IOException("OfflineReason invalid value: $value")
         }
     }
 }
