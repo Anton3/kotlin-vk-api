7e1d2b38e63c8302a411aadc7b5fd6348eaccf43
Anton Zhilin <antonyzhilin@gmail.com>
2018-12-24 10:09:25 +0300

    Fix parsing bug with VkList<T>

--- vk-api-core/src/main/kotlin/name/anton3/vkapi/vktypes/VkList.kt
+++ vk-api-core/src/main/kotlin/name/anton3/vkapi/vktypes/VkList.kt
@@ -1,3 +1,6 @@
 package name.anton3.vkapi.vktypes
 
+import com.fasterxml.jackson.annotation.JsonFormat
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 data class VkList<T>(val count: Int, val items: List<T>) : List<T> by items
--- vk-api-methods/src/test/kotlin/name/anton3/vkapi/methods/longpoll/UserLongPollEventSourceTest.kt
+++ vk-api-methods/src/test/kotlin/name/anton3/vkapi/methods/longpoll/UserLongPollEventSourceTest.kt
@@ -1,6 +1,9 @@
 package name.anton3.vkapi.methods.longpoll
 
 import kotlinx.coroutines.runBlocking
+import name.anton3.vkapi.client.invoke
+import name.anton3.vkapi.generated.messages.methods.MessagesGetById
+import name.anton3.vkapi.methods.longpoll.events.MessageAdded
 import name.anton3.vkapi.utils.groupApi
 import name.anton3.vkapi.utils.timeout
 import org.junit.Ignore
@@ -14,6 +17,11 @@ class UserLongPollEventSourceTest {
 
         for (event in source) {
             println(event.toString())
+
+            if (event is MessageAdded) {
+                val fullMessage = groupApi(MessagesGetById(messageIds = listOf(event.messageId)))
+                println(fullMessage.toString())
+            }
         }
     }
 }
