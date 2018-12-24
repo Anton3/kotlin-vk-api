19c01de8e868671293e084bb0497a7f564b5a8ef
Anton Zhilin <antonyzhilin@gmail.com>
2018-12-24 13:53:29 +0300

    Fix Unit parsing

--- vk-api-core/src/main/kotlin/name/anton3/vkapi/json/VkCoreSerialModule.kt
+++ vk-api-core/src/main/kotlin/name/anton3/vkapi/json/VkCoreSerialModule.kt
@@ -1,10 +1,7 @@
 package name.anton3.vkapi.json
 
 import com.fasterxml.jackson.databind.module.SimpleModule
-import name.anton3.vkapi.json.deser.BatchExecuteResultDeserializer
-import name.anton3.vkapi.json.deser.UnixTimeDeserializer
-import name.anton3.vkapi.json.deser.VkBirthDateDeserializer
-import name.anton3.vkapi.json.deser.VkResponseDeserializer
+import name.anton3.vkapi.json.deser.*
 import name.anton3.vkapi.methods.execute.BatchExecuteResult
 import name.anton3.vkapi.vktypes.VkBirthDate
 import name.anton3.vkapi.vktypes.VkDate
@@ -14,8 +11,9 @@ class VkCoreSerialModule : SimpleModule() {
     init {
         addDeserializer(VkDate::class.java, UnixTimeDeserializer)
         addDeserializer(VkBirthDate::class.java, VkBirthDateDeserializer)
-        addDeserializer(VkResponse::class.java, VkResponseDeserializer())
+        addDeserializer(Unit::class.java, UnitDeserializer)
 
+        addDeserializer(VkResponse::class.java, VkResponseDeserializer())
         addDeserializer(BatchExecuteResult::class.java, BatchExecuteResultDeserializer)
     }
 }
