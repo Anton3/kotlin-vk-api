288ebdaf46ed28d53a31b0859cd10036423b6ad6
Anton Zhilin <antonyzhilin@gmail.com>
2018-12-26 20:24:07 +0300

    Remove ExtraFields, because Jackson has poor support for embedded objects

--- vk-api-methods/src/main/kotlin/name/anton3/vkapi/json/VkObjectMapper.kt
+++ vk-api-methods/src/main/kotlin/name/anton3/vkapi/json/VkObjectMapper.kt
@@ -7,7 +7,6 @@ import name.anton3.vkapi.generated.wall.objects.CommentAttachment
 import name.anton3.vkapi.generated.wall.objects.WallpostAttachment
 import name.anton3.vkapi.json.deser.*
 import name.anton3.vkapi.methods.longpoll.events.LongPollEvent
-import name.anton3.vkapi.methods.longpoll.objects.ExtraFields
 import name.anton3.vkapi.methods.longpoll.objects.Flags
 
 class VkMainSerialModule : SimpleModule() {
@@ -16,7 +15,6 @@ class VkMainSerialModule : SimpleModule() {
         addDeserializer(CommentAttachment::class.java, VkCommentAttachmentsDeserializer)
         addDeserializer(MessageAttachment::class.java, VkMessageAttachmentsDeserializer)
 
-        addDeserializer(ExtraFields::class.java, ExtraFieldsDeserializer)
         addDeserializer(LongPollEvent::class.java, LongPollEventDeserializer)
         addDeserializer(Flags::class.java, FlagsDeserializer)
     }
--- vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/events/MessageAdded.kt
+++ vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/events/MessageAdded.kt
@@ -1,19 +1,15 @@
 package name.anton3.vkapi.methods.longpoll.events
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonUnwrapped
-import name.anton3.vkapi.methods.longpoll.objects.ExtraFields
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import name.anton3.vkapi.methods.longpoll.objects.Flags
+import name.anton3.vkapi.vktypes.VkDate
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class MessageAdded(
     override val eventType: Int,
     val messageId: Long,
     val flags: Flags,
-    @field:JsonIgnore val extra: ExtraFields?
-) : LongPollEvent(eventType) {
-
-    // Workaround for broken Jackson @JsonUnwrapped
-    @get:JsonUnwrapped
-    private val _extra: ExtraFields?
-        get() = extra
-}
+    val peerId: Long?,
+    val timestamp: VkDate?,
+    val text: String?
+) : LongPollEvent(eventType)
