c59ac1fbf0babb688513abe9e1f8812010df31ab
Anton Zhilin <antonyzhilin@gmail.com>
2018-12-31 17:32:34 +0300

    Add ref and ref_source to Message object

--- vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/messages/objects/Message.kt
+++ vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/messages/objects/Message.kt
@@ -7,35 +7,41 @@ import name.anton3.vkapi.vktypes.VkDate
  * No description
  *
  * @property id Message ID
- * @property conversationMessageId Unique auto-incremented number for all messages with this peer
  * @property date Date when the message has been sent in Unixtime
  * @property peerId Peer ID
  * @property fromId Message author's ID
- * @property updateTime Date when the message has been updated in Unixtime
+ * @property text Message text
  * @property randomId ID used for sending messages. It returned only for outgoing messages
+ * @property ref Arbitrary parameter for working with referring sources
+ * @property refSource Arbitrary parameter for working with referring sources
+ * @property attachments No description
  * @property important Is it an important message
+ * @property geo No description
  * @property payload No description
  * @property fwdMessages Forwarded messages
- * @property text Message text
- * @property attachments No description
- * @property geo No description
+ * @property replyMessage No description
  * @property action No description
+ * @property conversationMessageId Unique auto-incremented number for all messages with this peer
+ * @property updateTime Date when the message has been updated in Unixtime
  * @property keyboard No description
  */
 data class Message(
     val id: Long,
-    val conversationMessageId: Long? = null,
     val date: VkDate,
     val peerId: Long,
     val fromId: Long,
-    val updateTime: Long? = null,
+    val text: String,
     val randomId: Long? = null,
+    val ref: String? = null,
+    val refSource: String? = null,
+    val attachments: List<MessageAttachment>? = null,
     val important: Boolean? = null,
+    val geo: Geo? = null,
     val payload: String? = null,
     val fwdMessages: List<Message>? = null,
-    val text: String,
-    val attachments: List<MessageAttachment>? = null,
-    val geo: Geo? = null,
+    val replyMessage: Message? = null,
     val action: MessageAction? = null,
+    val conversationMessageId: Long? = null,
+    val updateTime: Long? = null,
     val keyboard: KeyboardReturned? = null
 )
