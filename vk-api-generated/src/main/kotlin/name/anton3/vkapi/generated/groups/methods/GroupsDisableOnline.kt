b4cd60e0e8fb452a460ab1ada1ec92917307bd31
Anton Zhilin <antonyzhilin@gmail.com>
2019-01-01 14:02:15 +0300

    Add groups.enableOnline

---
+++ vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/groups/methods/GroupsDisableOnline.kt
@@ -0,0 +1,20 @@
+@file:Suppress("unused", "MemberVisibilityCanBePrivate", "SpellCheckingInspection")
+
+package name.anton3.vkapi.generated.groups.methods
+
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import name.anton3.vkapi.core.VkMethod
+import name.anton3.vkapi.tokens.UserGroupMethod
+import name.anton3.vkapi.vktypes.OkResponse
+
+/**
+ * [https://vk.com/dev/groups.disableOnline]
+ *
+ * Disables 'online' status in a community
+ *
+ * @property groupId Group ID
+ */
+data class GroupsDisableOnline(
+    var groupId: Long
+) : VkMethod<OkResponse>("groups.disableOnline", jacksonTypeRef()),
+    UserGroupMethod
---
+++ vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/groups/methods/GroupsEnableOnline.kt
@@ -0,0 +1,20 @@
+@file:Suppress("unused", "MemberVisibilityCanBePrivate", "SpellCheckingInspection")
+
+package name.anton3.vkapi.generated.groups.methods
+
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import name.anton3.vkapi.core.VkMethod
+import name.anton3.vkapi.tokens.UserGroupMethod
+import name.anton3.vkapi.vktypes.OkResponse
+
+/**
+ * [https://vk.com/dev/groups.enableOnline]
+ *
+ * Enables 'online' status in a community
+ *
+ * @property groupId Group ID
+ */
+data class GroupsEnableOnline(
+    var groupId: Long
+) : VkMethod<OkResponse>("groups.enableOnline", jacksonTypeRef()),
+    UserGroupMethod
