48b8ce443b47421c9db65332bc7663b38c43b2f0
Anton Zhilin <antonyzhilin@gmail.com>
2019-01-21 17:09:11 +0300

    CoroutineContext not needed in DistinctExecutor

--- vk-api-core/src/main/kotlin/name/anton3/vkapi/rate/DistinctExecutor.kt
+++ vk-api-core/src/main/kotlin/name/anton3/vkapi/rate/DistinctExecutor.kt
@@ -1,15 +1,12 @@
 package name.anton3.vkapi.rate
 
 import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlin.coroutines.CoroutineContext
 
 class DistinctExecutor<Request, Response>(
-    private val base: Executor<Request, Response>,
-    override val coroutineContext: CoroutineContext
-) : Executor<Request, Response>, CoroutineScope {
+    private val base: Executor<Request, Response>
+) : Executor<Request, Response> {
 
     private val pendingRequests: MutableMap<Request, CompletableDeferred<Response>> = mutableMapOf()
     private val mutex: Mutex = Mutex()
