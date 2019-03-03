6d897e2ccbf67de7dabe29b13ad377b0069fa72c
Anton Zhilin <antonyzhilin@gmail.com>
2019-03-03 23:13:13 +0300

    Add getInviteLink method

---
+++ vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/get/objects/InviteLinkResponse.kt
@@ -0,0 +1,10 @@
+package name.anton3.vkapi.generated.get.objects
+
+/**
+ * No description
+ *
+ * @property link Invitation link for a given chat
+ */
+data class InviteLinkResponse(
+    val link: String
+)
---
+++ vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/messages/methods/MessagesGetInviteLink.kt
@@ -0,0 +1,24 @@
+@file:Suppress("unused", "MemberVisibilityCanBePrivate", "SpellCheckingInspection")
+
+package name.anton3.vkapi.generated.messages.methods
+
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import name.anton3.vkapi.core.VkMethod
+import name.anton3.vkapi.generated.get.objects.InviteLinkResponse
+import name.anton3.vkapi.tokens.UserGroupMethod
+
+/**
+ * [https://vk.com/dev/messages.getInviteLink]
+ *
+ * Get an invitation link for a chat
+ *
+ * @property peerId Destination ID
+ * @property reset Pass 1 to generate a new link and reset previous one
+ * @property groupId For actions on behalf of a group with user token
+ */
+data class MessagesGetInviteLink(
+    var peerId: Int,
+    var reset: Boolean? = null,
+    var groupId: Int? = null
+) : VkMethod<InviteLinkResponse>("messages.getInviteLink", jacksonTypeRef()),
+    UserGroupMethod
