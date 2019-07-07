b6393184886f4a81d3ae150e339b02af676e958c
Anton Zhilin <antonyzhilin@gmail.com>
2019-07-07 13:44:00 +0300

    Add missing MessagesSend parameters

--- vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/messages/methods/MessagesSend.kt
+++ vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/messages/methods/MessagesSend.kt
@@ -22,11 +22,15 @@ import name.anton3.vkapi.tokens.UserGroupMethod
  * @property lat Geographical latitude of a check-in, in degrees (from -90 to 90).
  * @property long Geographical longitude of a check-in, in degrees (from -180 to 180).
  * @property attachment (Required if 'message' is not set.) List of objects attached to the message, separated by commas, in the following format: "<owner_id>_<media_id>", '' — Type of media attachment: 'photo' — photo, 'video' — video, 'audio' — audio, 'doc' — document, 'wall' — wall post, '<owner_id>' — ID of the media attachment owner. '<media_id>' — media attachment ID. Example: "photo100172_166443618"
+ * @property replyTo Id of replied message.
  * @property forwardMessages ID of forwarded messages, separated with a comma. Listed messages of the sender will be shown in the message body at the recipient's. Example: "123,431,544"
  * @property stickerId Sticker id.
- * @property notification '1' if the message is a notification (for community messages).
  * @property groupId Group ID (for group messages with group access token)
  * @property keyboard keyboard to message
+ * @property payload Hidden message (escaped JSON)
+ * @property dontParseLinks 1 - links will not attach snippet
+ * @property disableMentions 1 - mention of user will not generate notification for him
+ * @property notification '1' if the message is a notification (for community messages).
  */
 data class MessagesSend(
     var userId: Int? = null,
@@ -39,10 +43,14 @@ data class MessagesSend(
     var lat: Double? = null,
     var long: Double? = null,
     var attachment: List<String>? = null,
+    var replyTo: Int? = null,
     var forwardMessages: String? = null,
     var stickerId: Int? = null,
-    var notification: Boolean? = null,
     var groupId: Int? = null,
-    var keyboard: Keyboard? = null
+    var keyboard: Keyboard? = null,
+    var payload: String? = null,
+    var dontParseLinks: Boolean? = null,
+    var disableMentions: Boolean? = null,
+    var notification: Boolean? = null
 ) : VkMethod<Int>("messages.send", jacksonTypeRef()),
     UserGroupMethod
