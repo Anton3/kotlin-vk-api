7288d7935ec840feab564fdba83ffd29da0761e2
Anton Zhilin <antonyzhilin@gmail.com>
2019-07-08 04:27:05 +0300

    Simplify VkDate

--- vk-api-core/src/main/kotlin/name/anton3/vkapi/vktypes/VkDate.kt
+++ vk-api-core/src/main/kotlin/name/anton3/vkapi/vktypes/VkDate.kt
@@ -3,9 +3,6 @@ package name.anton3.vkapi.vktypes
 import com.fasterxml.jackson.annotation.JsonValue
 import java.util.*
 
-class VkDate(@JsonValue private val unixTimeSeconds: Long) {
-
-    fun toDate(): Date = Date(unixTimeSeconds * 1000)
-
+class VkDate(@JsonValue private val unixTimeSeconds: Long) : Date(unixTimeSeconds * 1000) {
     override fun toString(): String = unixTimeSeconds.toString()
 }
