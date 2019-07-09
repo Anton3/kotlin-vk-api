2a964bb526e655f1b97acb7bd98bd5406d09732b
Anton Zhilin <antonyzhilin@gmail.com>
2019-07-09 23:41:07 +0300

    Hotfix: VkClientFactory was unusable due to improper Collection choice

--- vk-api-core/src/main/kotlin/name/anton3/vkapi/client/VkClientFactory.kt
+++ vk-api-core/src/main/kotlin/name/anton3/vkapi/client/VkClientFactory.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import name.anton3.vkapi.core.MethodExecutor
 import name.anton3.vkapi.core.TransportClient
@@ -16,7 +18,6 @@ import name.anton3.vkapi.tokens.ServiceToken
 import name.anton3.vkapi.tokens.UserToken
 import java.io.Closeable
 import java.time.Duration
-import java.util.concurrent.ConcurrentSkipListSet
 import kotlin.coroutines.CoroutineContext
 
 class VkClientFactory(
@@ -27,9 +28,10 @@ class VkClientFactory(
     private val job = Job(parentContext[Job])
     private val context = parentContext + job
     private val baseExecutor: MethodExecutor = JsonApiMethodExecutor(transportClient, objectMapper)
-    private val closeableExecutors: ConcurrentSkipListSet<Closeable> = ConcurrentSkipListSet()
+    private val closeableExecutors: MutableList<Closeable> = mutableListOf()
+    private val closeableExecutorsMutex: Mutex = Mutex()
 
-    fun user(
+    suspend fun user(
         token: UserToken,
         flushDelayMillis: Long = 500L,
         executorWrapper: (MethodExecutor) -> MethodExecutor = { it }
@@ -37,11 +39,11 @@ class VkClientFactory(
         val withToken = TokenMethodExecutor(baseExecutor, token)
         val throttled = ThrottledMethodExecutor(withToken, context, 3)
         val batch = BatchMethodExecutor(throttled, context, Duration.ofMillis(flushDelayMillis))
-        closeableExecutors.addAll(listOf(throttled, batch))
+        closeableExecutorsMutex.withLock { closeableExecutors.addAll(listOf(throttled, batch)) }
         return UserClient(executorWrapper(batch))
     }
 
-    fun group(
+    suspend fun group(
         token: GroupToken,
         flushDelayMillis: Long = 60L,
         executorWrapper: (MethodExecutor) -> MethodExecutor = { it }
@@ -49,11 +51,11 @@ class VkClientFactory(
         val withToken = TokenMethodExecutor(baseExecutor, token)
         val throttled = ThrottledMethodExecutor(withToken, context, 20)
         val batch = BatchMethodExecutor(throttled, context, Duration.ofMillis(flushDelayMillis))
-        closeableExecutors.addAll(listOf(throttled, batch))
+        closeableExecutorsMutex.withLock { closeableExecutors.addAll(listOf(throttled, batch)) }
         return GroupClient(executorWrapper(batch))
     }
 
-    fun ads(
+    suspend fun ads(
         token: UserToken,
         serviceType: AdServiceType = AdServiceType.NORMAL,
         executorWrapper: (MethodExecutor) -> MethodExecutor = { it }
@@ -61,24 +63,25 @@ class VkClientFactory(
         val withToken = TokenMethodExecutor(baseExecutor, token)
         val throttled1 = ThrottledMethodExecutor(withToken, context, 2)
         val throttled2 = ThrottledMethodExecutor(throttled1, context, serviceType.requestsPerHour, Duration.ofHours(1))
-        closeableExecutors.addAll(listOf(throttled1, throttled2))
+        closeableExecutorsMutex.withLock { closeableExecutors.addAll(listOf(throttled1, throttled2)) }
         return UserClient(executorWrapper(throttled2))
     }
 
-    fun secure(
+    suspend fun secure(
         token: ServiceToken,
         appPopularity: SecureAppPopularity = SecureAppPopularity.SMALL,
         executorWrapper: (MethodExecutor) -> MethodExecutor = { it }
     ): ServiceClient {
         val withToken = TokenMethodExecutor(baseExecutor, token)
         val throttled = ThrottledMethodExecutor(withToken, context, appPopularity.requestsPerSecond)
-        closeableExecutors.add(throttled)
+        closeableExecutorsMutex.withLock { closeableExecutors.add(throttled) }
         return ServiceClient(executorWrapper(throttled))
     }
 
     suspend fun closeAndJoin() {
+        val closeableStuff = closeableExecutorsMutex.withLock { closeableExecutors.toList() }
         withContext(Dispatchers.IO) {
-            closeableExecutors.clone().reversed().forEach { it.close() }
+            closeableStuff.reversed().forEach { it.close() }
         }
         job.cancelAndJoin()
     }
