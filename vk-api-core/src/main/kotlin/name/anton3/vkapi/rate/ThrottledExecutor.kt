051907366d47540622c929b7a95217b3606cd9e0
Anton Zhilin <antonyzhilin@gmail.com>
2019-07-11 17:03:17 +0300

    Simplify ThrottledExecutor implementation using Semaphore

--- vk-api-core/src/main/kotlin/name/anton3/vkapi/rate/ThrottledExecutor.kt
+++ vk-api-core/src/main/kotlin/name/anton3/vkapi/rate/ThrottledExecutor.kt
@@ -2,15 +2,14 @@ package name.anton3.vkapi.rate
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ClosedReceiveChannelException
-import kotlinx.coroutines.channels.ClosedSendChannelException
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.time.delay
 import java.io.Closeable
 import java.time.Duration
-import java.time.Instant
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
 import kotlin.coroutines.CoroutineContext
 
 class ThrottledExecutor<Request, Response>(
@@ -24,82 +23,44 @@ class ThrottledExecutor<Request, Response>(
     private val coroutineContext: CoroutineContext = coroutineContext + job
     private val coroutineScope: CoroutineScope = CoroutineScope(this.coroutineContext)
 
-    private val tickets: Channel<Instant> = Channel(rateLimit)
-    private val requests: MutableSet<CompletableRequest<Request, Response>> = LinkedHashSet()
-    private val newRequests: Channel<CompletableRequest<Request, Response>> = Channel()
+    private val timeTickets: Semaphore = Semaphore(rateLimit)
+    private val requestTickets: Semaphore = Semaphore(Int.MAX_VALUE, Int.MAX_VALUE)
+    private val requests: Queue<CompletableRequest<Request, Response>> = ConcurrentLinkedQueue()
 
     init {
         require(rateLimit > 0 && ratePeriod > Duration.ZERO)
 
-        val now = Instant.now()
-        repeat(rateLimit) { tickets.offer(now) }
-
         coroutineScope.launch {
-            while (isActive) {
-                if (!receiveTicket()) break
-                if (!receiveRequest()) break
-                val request = takeRequest()
-                sendRequest(request)
-            }
+            while (isActive) handleNextRequest()
         }
     }
 
-    override suspend fun execute(dynamicRequest: DynamicRequest<Request>): Response = dynamicRequest.submit(coroutineContext) {
-        newRequests.send(it)
-    }
-
-    private suspend fun receiveTicket(): Boolean {
-        val ticket = try {
-            tickets.receive()
-        } catch (e: ClosedReceiveChannelException) {
-            return false
-        }
-
-        val now = Instant.now()
-        if (ticket > now) delay(Duration.between(now, ticket))
-        return true
-    }
-
-    private suspend fun receiveRequest(): Boolean {
-        return try {
-            var newRequest = newRequests.poll()
-            while (newRequest != null) {
-                requests.add(newRequest)
-                newRequest = newRequests.poll()
-            }
-            if (requests.isEmpty()) {
-                newRequest = newRequests.receive()
-                requests.add(newRequest)
-            }
-            true
-        } catch (e: ClosedReceiveChannelException) {
-            requests.isNotEmpty()
+    override suspend fun execute(dynamicRequest: DynamicRequest<Request>): Response {
+        return dynamicRequest.submit(coroutineContext) {
+            requests.add(it)
+            requestTickets.release()
         }
     }
 
-    private fun takeRequest(): CompletableRequest<Request, Response> {
+    private suspend fun handleNextRequest() {
+        timeTickets.acquire()
+        requestTickets.acquire()
         val request = requests.find { !it.request.isIncompleteBatch } ?: requests.first()
         requests.remove(request)
-        return request
-    }
 
-    private fun sendRequest(completableRequest: CompletableRequest<Request, Response>) {
         coroutineScope.launch {
             try {
-                base.complete(completableRequest)
+                base.complete(request)
             } finally {
-                try {
-                    tickets.send(Instant.now() + ratePeriod)
-                } catch (e: ClosedSendChannelException) {
-                    // Executor is closed
+                coroutineScope.launch {
+                    delay(ratePeriod)
+                    timeTickets.release()
                 }
             }
         }
     }
 
     override fun close() {
-        newRequests.cancel()
-        tickets.close()
         job.cancel()
     }
 }
