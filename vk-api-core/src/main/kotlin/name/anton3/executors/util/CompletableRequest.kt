47cc26895e700d063833b525e460a69642cb891f
Anton Zhilin <antonyzhilin@gmail.com>
2019-08-01 21:23:40 +0300

    Cleanup implementation

--- vk-api-core/src/main/kotlin/name/anton3/executors/util/CompletableRequest.kt
+++ vk-api-core/src/main/kotlin/name/anton3/executors/util/CompletableRequest.kt
@@ -1,30 +1,13 @@
 package name.anton3.executors.util
 
 import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.withContext
 import name.anton3.executors.core.DynamicExecutor
 import name.anton3.executors.core.DynamicRequest
-import kotlin.coroutines.CoroutineContext
 
 class CompletableRequest<out Request, Response>(
     val request: DynamicRequest<Request>,
     private val handle: CompletableDeferred<Response>
 ) : CompletableDeferred<Response> by handle
-
-internal suspend fun <Request, Response> DynamicRequest<Request>.submit(
-    coroutineContext: CoroutineContext,
-    block: suspend (CompletableRequest<Request, Response>) -> Unit
-): Response {
-    return withContext(coroutineContext) {
-        val completable = CompletableRequest<Request, Response>(
-            request = this@submit,
-            handle = CompletableDeferred(coroutineContext[Job])
-        )
-        block(completable)
-        completable.await()
-    }
-}
 
 internal suspend inline fun <Request, Response> DynamicExecutor<Request, Response>.complete(
     completableRequest: CompletableRequest<Request, Response>
