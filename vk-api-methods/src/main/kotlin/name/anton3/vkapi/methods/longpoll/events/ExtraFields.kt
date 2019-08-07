a013bf0b8e39972ab43a3b656e662be00bdfc56f
Anton Zhilin <antonyzhilin@gmail.com>
2019-08-07 20:35:34 +0300

    Replace ChatAction with MessageActionStatus, which is the same, but supports all actions

--- vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/events/ExtraFields.kt
+++ vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/events/ExtraFields.kt
@@ -1,14 +1,14 @@
 package name.anton3.vkapi.methods.longpoll.events
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import name.anton3.vkapi.methods.longpoll.objects.ChatAction
+import name.anton3.vkapi.generated.messages.objects.MessageActionStatus
 import name.anton3.vkapi.vktypes.PropertyExists
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class ExtraFields(
     val title: String?,
     val from: Int?,
-    val sourceAct: ChatAction?,
+    val sourceAct: MessageActionStatus?,
     val sourceMid: Int?,
     val fromAdmin: Int?,
     val geo: String?,
