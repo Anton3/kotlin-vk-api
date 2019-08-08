51b9a5a8bf23206847b017c489de6ea83c03d0b7
Anton Zhilin <antonyzhilin@gmail.com>
2019-08-08 18:18:26 +0300

    Fix equals & hashCode in VkDate

--- vk-api-core/src/main/kotlin/name/anton3/vkapi/vktypes/VkDate.kt
+++ vk-api-core/src/main/kotlin/name/anton3/vkapi/vktypes/VkDate.kt
@@ -4,5 +4,8 @@ import com.fasterxml.jackson.annotation.JsonValue
 import java.util.*
 
 class VkDate(@JsonValue private val unixTimeSeconds: Long) : Date(unixTimeSeconds * 1000) {
+
+    override fun equals(other: Any?): Boolean = other is VkDate && unixTimeSeconds == other.unixTimeSeconds
+    override fun hashCode(): Int = unixTimeSeconds.hashCode()
     override fun toString(): String = unixTimeSeconds.toString()
 }
