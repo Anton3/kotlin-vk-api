54a6a2e32a6c83a07ac655de9fad853cafc4c85a
Anton Zhilin <antonyzhilin@gmail.com>
2019-11-14 02:08:00 +0300

    Payload in User LongPoll

--- vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/objects/ExtraFields.kt
+++ vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/objects/ExtraFields.kt
@@ -16,5 +16,6 @@ data class ExtraFields(
     val fromAdmin: Int?,
     val geo: String?,
     @JsonFormat(shape = JsonFormat.Shape.STRING)
-    val emoji: Int?
+    val emoji: Int?,
+    val payload: String?
 )
