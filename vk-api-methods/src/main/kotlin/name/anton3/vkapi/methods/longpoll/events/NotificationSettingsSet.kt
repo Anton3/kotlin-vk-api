cc4f2fbdaba19882402c07a0efc48f8e97c07909
Anton Zhilin <antonyzhilin@gmail.com>
2019-11-16 03:00:16 +0300

    Fix NotificationSettingsSet User LongPoll event

--- vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/events/NotificationSettingsSet.kt
+++ vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/events/NotificationSettingsSet.kt
@@ -1,14 +1,8 @@
 package name.anton3.vkapi.methods.longpoll.events
 
+import name.anton3.vkapi.generated.account.objects.PushConversationsItem
+
 data class NotificationSettingsSet(
     override val eventType: Int,
-    val peerId: Int,
-    val sound: Boolean,
-    val disabledUntil: Int
-) : LongPollEvent(eventType) {
-
-    companion object {
-        const val ENABLED: Long = 0L
-        const val FOREVER: Long = -1L
-    }
-}
+    val extra: PushConversationsItem
+) : LongPollEvent(eventType)
