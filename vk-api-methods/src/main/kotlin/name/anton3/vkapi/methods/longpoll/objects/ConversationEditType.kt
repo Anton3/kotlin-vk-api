a71bd1174b7f92ab9930252cd395b4fa3217c7b1
Anton Zhilin <antonyzhilin@gmail.com>
2019-11-18 21:01:35 +0300

    Add a missing ConversationEditType

--- vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/objects/ConversationEditType.kt
+++ vk-api-methods/src/main/kotlin/name/anton3/vkapi/methods/longpoll/objects/ConversationEditType.kt
@@ -6,12 +6,13 @@ enum class ConversationEditType(override val value: Int) : ValueEnum<Int> {
     TITLE_CHANGE(1),
     PHOTO_CHANGE(2),
     ADMIN_ADDED(3),
-    UNKNOWN(4),  // Not described in spec, but happens
+    UNKNOWN(4),
     MESSAGE_PINNED(5),
     USER_JOINED(6),
     USER_EXITED(7),
     USER_KICKED(8),
     ADMIN_REMOVED(9),
-    UNKNOWN2(10),  // Not described in spec, but happens
-    UNKNOWN3(11);  // Not described in spec, but happens
+    UNKNOWN2(10),
+    UNKNOWN3(11),
+    UNKNOWN4(18)
 }
