781f017574bac61882a96be48824fe2844b31a4c
Anton Zhilin <antonyzhilin@gmail.com>
2019-11-22 12:29:36 +0300

    Add a missing LinkButtonActionType

--- vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/common/objects/LinkButtonActionType.kt
+++ vk-api-generated/src/main/kotlin/name/anton3/vkapi/generated/common/objects/LinkButtonActionType.kt
@@ -5,5 +5,6 @@ package name.anton3.vkapi.generated.common.objects
 import name.anton3.vkapi.vktypes.ValueEnum
 
 enum class LinkButtonActionType(override val value: String) : ValueEnum<String> {
-    OPEN_URL("open_url")
+    OPEN_URL("open_url"),
+    JOIN_GROUP_AND_OPEN_URL("join_group_and_open_url")
 }
