8341e4798a164f18bc92bd0ac3e165e714dfc772
Anton Zhilin <antonyzhilin@gmail.com>
2020-02-12 22:46:06 +0300

    Fix MultipartForm handling in ApacheTransportClient
    MultipartFormEntity unnecessarily complains about form size

--- vk-api-apache/src/main/kotlin/name/anton3/vkapi/transport/ApacheTransportClient.kt
+++ vk-api-apache/src/main/kotlin/name/anton3/vkapi/transport/ApacheTransportClient.kt
@@ -15,6 +15,9 @@ import org.apache.http.entity.mime.MultipartEntityBuilder
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
 import org.apache.http.nio.client.HttpAsyncClient
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
@@ -68,18 +71,22 @@ class ApacheTransportClient(private val client: HttpAsyncClient) : TransportClie
             val builder = MultipartEntityBuilder.create()
             builder.setContentType(ContentType.create("multipart/form-data", Charsets.UTF_8))
             for (part in body.parts) {
-                when (part) {
-                    is TransportRequest.Part.Text -> {
-                        val contentType = ContentType.create("text/plain", Charsets.UTF_8)
-                        builder.addTextBody(part.key, part.value, contentType)
-                    }
-                    is TransportRequest.Part.File -> {
-                        val contentType = ContentType.create("application/octet-stream", Charsets.UTF_8)
-                        builder.addBinaryBody(part.key, part.data, contentType, part.fileName)
-                    }
-                }
+                addPart(builder, part)
             }
-            builder.build()
+            NonComplainingMultipartEntity(builder.build())
+        }
+    }
+
+    private fun addPart(multipartBuilder: MultipartEntityBuilder, part: TransportRequest.Part) {
+        when (part) {
+            is TransportRequest.Part.Text -> {
+                val contentType = ContentType.create("text/plain", Charsets.UTF_8)
+                multipartBuilder.addTextBody(part.key, part.value, contentType)
+            }
+            is TransportRequest.Part.File -> {
+                val contentType = ContentType.create("application/octet-stream", Charsets.UTF_8)
+                multipartBuilder.addBinaryBody(part.key, part.data, contentType, part.fileName)
+            }
         }
     }
 
@@ -106,4 +113,19 @@ fun defaultHttpAsyncClient(): CloseableHttpAsyncClient {
         .disableCookieManagement()
         .build()
         .apply { start() }
+}
+
+
+private class ExtractableByteArrayOutputStream(size: Int) : ByteArrayOutputStream(size) {
+    val buffer: ByteArray
+        get() = buf
+}
+
+private class NonComplainingMultipartEntity(base: HttpEntity) : HttpEntity by base {
+    override fun getContent(): InputStream {
+        val out = ExtractableByteArrayOutputStream(this.contentLength.toInt())
+        writeTo(out)
+        out.flush()
+        return ByteArrayInputStream(out.buffer)
+    }
 }
