fc292b6f2975bfcb31006cf3e46fec825fa250c5
Anton Zhilin <antonyzhilin@gmail.com>
2020-02-16 00:31:01 +0300

    Fix occasional errors in StoringDynamicExecutor
    Turns out, we need to store the exact requests passed to us in requestStorage

--- executors/src/main/kotlin/name/anton3/executors/instances/StoringDynamicExecutor.kt
+++ executors/src/main/kotlin/name/anton3/executors/instances/StoringDynamicExecutor.kt
@@ -6,17 +6,19 @@ import name.anton3.executors.core.DynamicExecutor
 import name.anton3.executors.core.DynamicRequest
 import name.anton3.executors.core.RequestStorage
 import name.anton3.executors.util.CompletableRequest
+import java.util.*
 
 abstract class StoringDynamicExecutor<Request, Response> : DynamicExecutor<Request, Response> {
 
     protected abstract val requestStorage: RequestStorage<Request>
     protected abstract val job: Job
 
+    private val handles: MutableMap<DynamicRequest<Request>, CompletableDeferred<Response>> =
+        Collections.synchronizedMap(WeakHashMap())
+
     protected suspend fun add(request: DynamicRequest<Request>): CompletableDeferred<Response> {
-        val handle = CompletableDeferred<Response>(parent = job)
-        val withHandle: DynamicRequest<Request> = CompletableRequestAsDynamic(CompletableRequest(request, handle))
-        requestStorage.add(withHandle)
-        return handle
+        requestStorage.add(request)
+        return CompletableDeferred<Response>(parent = job).also { handles[request] = it }
     }
 
     protected suspend inline fun submit(request: DynamicRequest<Request>, block: () -> Unit): Response {
@@ -27,31 +29,11 @@ abstract class StoringDynamicExecutor<Request, Response> : DynamicExecutor<Reque
 
     protected suspend fun poll(): CompletableRequest<Request, Response> {
         val request = requestStorage.poll()
-        @Suppress("UNCHECKED_CAST")
-        return request[CompletableRequestKey] as CompletableRequest<Request, Response>
+        return CompletableRequest(request, handles[request]!!)
     }
 
     protected suspend fun pollMany(n: Int): List<CompletableRequest<Request, Response>> {
         val requests = requestStorage.pollMany(n)
-        @Suppress("UNCHECKED_CAST")
-        return requests.map { it[CompletableRequestKey] as CompletableRequest<Request, Response> }
-    }
-
-    private object CompletableRequestKey : DynamicRequest.Key<CompletableRequest<*, *>>
-
-    private class CompletableRequestAsDynamic<Request, Response>(
-        private val completableRequest: CompletableRequest<Request, Response>
-    ) : DynamicRequest<Request> {
-
-        override suspend fun get(): Request = completableRequest.request.get()
-
-        override fun <T : Any> get(key: DynamicRequest.Key<T>): T? {
-            return if (key is CompletableRequestKey) {
-                @Suppress("UNCHECKED_CAST")
-                completableRequest as T
-            } else {
-                completableRequest.request[key]
-            }
-        }
+        return requests.map { CompletableRequest(it, handles[it]!!) }
     }
 }
