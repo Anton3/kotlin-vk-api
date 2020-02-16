a1806640caaaa6b78c527de4ade9b36bec6b7018
Anton Zhilin <antonyzhilin@gmail.com>
2020-02-16 14:10:55 +0300

    Add a test for BatchExecutor and ThrottledExecutor

--- executors/build.gradle.kts
+++ executors/build.gradle.kts
@@ -3,3 +3,7 @@ dependencies {
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutines")
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
--- executors/src/test/kotlin/name/anton3/executors/instances/SourceLimitingExecutorTest.kt
+++ executors/src/test/kotlin/name/anton3/executors/instances/SourceLimitingExecutorTest.kt
@@ -7,12 +7,52 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.time.delay
 import name.anton3.executors.core.DynamicExecutor
 import name.anton3.executors.core.DynamicRequest
+import name.anton3.executors.core.map
+import name.anton3.executors.util.FifoRequestStorage
 import org.junit.jupiter.api.Test
 import java.time.Duration
 
 class SourceLimitingExecutorTest {
     @Test
-    fun testLimitedHistory() = runBlocking<Unit>(context = Dispatchers.Default) {
+    fun testThrottledExecutor(): Unit = runBlocking<Unit>(context = Dispatchers.Default) {
+        val baseExecutor = object : DynamicExecutor<String, Unit> {
+            override suspend fun execute(dynamicRequest: DynamicRequest<String>) {
+                println(dynamicRequest.get())
+            }
+        }
+
+        val throttler = ThrottledExecutor(
+            base = baseExecutor,
+            coroutineContext = Dispatchers.Default,
+            rateLimit = 3,
+            ratePeriod = Duration.ofSeconds(1),
+            requestStorage = FifoRequestStorage()
+        )
+
+        val batcher = BatchExecutor(
+            base = object : DynamicExecutor<List<String>, List<Unit>> {
+                override suspend fun execute(dynamicRequest: DynamicRequest<List<String>>): List<Unit> {
+                    throttler.execute(dynamicRequest.map { "batch: " + it.joinToString() })
+                    return List(dynamicRequest.get().size) { Unit }
+                }
+            },
+            coroutineContext = Dispatchers.Default,
+            batchSize = 3,
+            flushDelay = Duration.ofSeconds(1),
+            requestStorage = FifoRequestStorage()
+        )
+
+        coroutineScope {
+            repeat(17) { i ->
+                launch {
+                    batcher.execute(i.toString())
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testLimitedHistory(): Unit = runBlocking<Unit>(context = Dispatchers.Default) {
         val baseExecutor = object : DynamicExecutor<String, Unit> {
             override suspend fun execute(dynamicRequest: DynamicRequest<String>) {
                 println(dynamicRequest.get())
@@ -32,7 +72,7 @@ class SourceLimitingExecutorTest {
 
                 launch {
                     for (j in 1..10) {
-                        delay(Duration.ofMillis(20))
+                        delay(Duration.ofMillis(30))
 
                         try {
                             keySettingExecutor.execute("$i $j")
