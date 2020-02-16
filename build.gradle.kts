9616c55a053c4e7d1bcb090309bf43493211a69c
Anton Zhilin <antonyzhilin@gmail.com>
2020-02-16 14:17:50 +0300

    Bump own version

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ tasks.withType(KotlinCompile::class.java).all {
 
 subprojects {
     group = "com.github.Anton3"
-    version = "0.12.0"
+    version = "0.12.2"
 
     repositories {
         mavenCentral()
