2780d5552d4e13e6973c581cd54fe5f39a54e397
Anton Zhilin <antonyzhilin@gmail.com>
2020-02-20 20:25:50 +0300

    Add convenience constructors of trivial Executor's and DynamicExecutor's

--- executors/src/main/kotlin/name/anton3/executors/core/DynamicExecutor.kt
+++ executors/src/main/kotlin/name/anton3/executors/core/DynamicExecutor.kt
@@ -11,3 +11,20 @@ interface DynamicExecutor<Request, Response> : Executor<Request, Response> {
         return execute(SimpleDynamicRequest(request))
     }
 }
+
+
+@Suppress("FunctionName")
+inline fun <Request, Response> Executor(
+    crossinline block: suspend (Request) -> Response
+): Executor<Request, Response> =
+    object : Executor<Request, Response> {
+        override suspend fun execute(request: Request): Response = block(request)
+    }
+
+@Suppress("FunctionName")
+inline fun <Request, Response> DynamicExecutor(
+    crossinline block: suspend (DynamicRequest<Request>) -> Response
+): DynamicExecutor<Request, Response> =
+    object : DynamicExecutor<Request, Response> {
+        override suspend fun execute(dynamicRequest: DynamicRequest<Request>): Response = block(dynamicRequest)
+    }
