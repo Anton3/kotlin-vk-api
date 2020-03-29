b2dd13453f5fea7bbdde55323c648dfb552ac964
Anton Zhilin <antonyzhilin@gmail.com>
2020-03-29 12:16:08 +0300

    Add 'captcha' errors

--- vk-api-base/src/main/kotlin/name/anton3/vkapi/vktypes/VkError.kt
+++ vk-api-base/src/main/kotlin/name/anton3/vkapi/vktypes/VkError.kt
@@ -6,7 +6,12 @@ data class VkError(
     val errorCode: Int,
     val errorMsg: String,
     val requestParams: List<MapEntry> = emptyList(),
-    val method: String? = null
+    val method: String? = null,
+    val errorText: String? = null,
+    val captchaSid: String? = null,
+    val captchaImg: String? = null,
+    val confirmationText: String? = null
+
 ) {
     data class MapEntry(val key: String, val value: JsonNode)
 
