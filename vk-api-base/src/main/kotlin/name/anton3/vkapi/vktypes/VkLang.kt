a04bb3f9d50b79f74cf5cc74d751334061ec3eb5
Anton Zhilin <antonyzhilin@gmail.com>
2020-03-29 12:13:18 +0300

    Add the new languages supported by VK

--- vk-api-base/src/main/kotlin/name/anton3/vkapi/vktypes/VkLang.kt
+++ vk-api-base/src/main/kotlin/name/anton3/vkapi/vktypes/VkLang.kt
@@ -1,5 +1,93 @@
 package name.anton3.vkapi.vktypes
 
 enum class VkLang(private val code: Int) {
-    RU(0), UA(1), BE(2), EN(3), ES(4), FI(5), DE(6), IT(7)
+    RU(0),  // Русский
+    UK(1),  // Українська
+    BE(2),  // Беларуская (тарашкевiца)
+    EN(3),  // English
+    ES(4),  // Español
+    FI(5),  // Suomi
+    DE(6),  // Deutsch
+    IT(7),  // Italiano
+    BG(8),  // Български
+    HR(9),  // Hrvatski
+    HU(10),  // Magyar
+    SR(11),  // Српски
+    PT(12),  // Português
+    EL(14),  // Ελληνικά
+    PL(15),  // Polski
+    FR(16),  // Français
+    KO(17),  // 한국어
+    ZH(18),  // 汉语
+    LT(19),  // Lietuvių
+    JA(20),  // 日本語
+    CS(21),  // Čeština
+    ET(22),  // Eesti
+    TT(50),  // Татарча
+    BA(51),  // Башҡортса
+    CV(52),  // Чăвашла
+    SK(53),  // Slovenčina
+    RO(54),  // Română
+    NO(55),  // Norsk
+    LV(56),  // Latviešu
+    AZ(57),  // Azərbaycan dili
+    HY(58),  // Հայերեն
+    SQ(59),  // Shqip
+    SV(60),  // Svenska
+    NL(61),  // Nederlands
+    TK(62),  // Türkmen
+    KA(63),  // ქართული
+    DA(64),  // Dansk
+    UZ(65),  // O‘zbek
+    MO(66),  // Moldovenească
+    BUA(67),  // Буряад
+    TH(68),  // ภาษาไทย
+    ID(69),  // Bahasa Indonesia
+    TG(70),  // Тоҷикӣ
+    SL(71),  // Slovenščina
+    BS(72),  // Bosanski
+    PTBR(73),  // Português brasileiro
+    FA(74),  // فارسی
+    VI(75),  // Tiếng Việt
+    HI(76),  // हिन्दी
+    SI(77),  // සිංහල
+    BN(78),  // বাংলা
+    TL(79),  // Tagalog
+    MN(80),  // Монгол
+    MY(81),  // ဗမာစာ
+    TR(82),  // Türkçe
+    NE(83),  // नेपाली
+    UR(85),  // اردو
+    KY(87),  // Кыргыз тили
+    PA(90),  // پنجابی
+    OS(91),  // Ирон
+    KN(94),  // ಕನ್ನಡ
+    SW(95),  // Kiswahili
+    KK(97),  // Қазақша
+    AR(98),  // العربية
+    HE(99),  // עברית
+    PreRevolutionary(100),  // Дореволюцiонный
+    MYV(101),  // Эрзянь кель
+    KDB(102),  // Адыгэбзэ
+    SAH(105),  // Саха тыла
+    ADY(106),  // Адыгабзэ
+    UDM(107),  // Удмурт
+    CHM(108),  // Марий йылме
+    BE2(114),  // Беларуская
+    LEZ(118),  // Лезги чІал
+    TW(119),  // 臺灣話
+    KUM(236),  // Къумукъ тил
+    MVL(270),  // Mirandés
+    SLA(298),  // Русинськый
+    KRL(379),  // Karjalan kieli
+    TYV(344),  // Тыва дыл
+    XAL(357),  // Хальмг келн
+    TLY(373),  // Tolışə zıvon
+    KV(375),  // Коми кыв
+    UKClassic(452),  // Українська (клясична)
+    UKGalitska(454),  // Українська (Галицка)
+    KAB(457),  // Taqbaylit
+    EO(555),  // Esperanto
+    LA(666),  // Lingua Latina
+    Soviet(777),  // Советский
 }
